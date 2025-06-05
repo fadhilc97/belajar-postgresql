622261405720428a46bae8201c0db9b5a8f408e0
Fadhil <fadhil@incit.org>
2025-06-05 11:35:37 +0700

    Melengkapi solusi setiap soal di file ke-14

--- 14-klausa-where.sql
+++ 14-klausa-where.sql
@@ -31,25 +31,70 @@ INSERT INTO "Siswa" VALUES
 SELECT * FROM "Siswa";
 
 /* 1. Siswa mana saja yang berjenis kelamin pria ? */
+SELECT "nama_lengkap", "jenis_kelamin" FROM "Siswa" WHERE "jenis_kelamin" = 'pria';
 
 /* 2. Siswa mana saja yang tinggi badannya minimal 150 cm ? */ 
+SELECT "nama_lengkap", "tinggi_badan" FROM "Siswa" WHERE "tinggi_badan" >= 150;
 
 /* 3. Untuk keperluan seleksi pertandingan bola basket antar siswa di tingkat kabupaten kategori putra, 
 pelatih menginginkan siswa yang berminat harus memiliki tinggi badan minimal 150 cm. 
 Siapakah yang berpotensi memenuhi persyaratan tersebut ? */
+SELECT 
+  "nama_lengkap", 
+  "jenis_kelamin", 
+  "tinggi_badan"
+FROM "Siswa"
+WHERE 
+  "jenis_kelamin" = 'pria' 
+  AND "tinggi_badan" >= 150;
 
 /* 4. Siswa mana saja yang memiliki berat badan berkisar antara 41 sampai dengan 45 kg ? */
+SELECT
+  "nama_lengkap",
+  "berat_badan"
+FROM "Siswa"
+WHERE
+  "berat_badan" BETWEEN 41 AND 45;
 
 /* 5. Siswa mana saja yang belum terdata alamat tempat tinggalnya ? */
+SELECT
+  "nama_lengkap",
+  "alamat"
+FROM
+  "Siswa"
+WHERE
+  "alamat" IS NULL;
 
 /* 6. Pelatih basket memiliki kendala dalam proses seleksi karena partisipasinya yang masih minim. 
 Rupanya, ada beberapa siswa yang belum terdata tinggi badannya. 
 Siapa saja siswa yang belum terdata tinggi badannya ? */
+SELECT 
+  "nama_lengkap",
+  "tinggi_badan"
+FROM "Siswa"
+WHERE
+  "tinggi_badan" IS NULL;
 
 /* 7. Berat badan ideal diukur dengan Index Massa Tubuh atau Body Mass Index (BMI). 
 Index tersebut diperoleh jika diketahui tinggi badan dan berat badan. 
 Dengan data saat ini, siapa saja yang dapat diketahui nilai BMI terlebih dahulu ? */
+SELECT
+  "nama_lengkap",
+  "tinggi_badan",
+  "berat_badan"
+FROM "Siswa"
+WHERE
+  "tinggi_badan" IS NOT NULL
+  AND "berat_badan" IS NOT NULL;
 
 /* 8. UKS di sekolah tersebut mengadakan pemeriksaan kesehatan rutin untuk seluruh siswa yang terdata. 
 Namun terdapat beberapa siswa yang belum terdata baik tinggi badan maupun berat badannya dan siswa tersebut akan diprioritaskan. 
 Siapa saja siswa yang mendapat prioritas tersebut ? */
+SELECT
+  "nama_lengkap",
+  "tinggi_badan",
+  "berat_badan"
+FROM "Siswa"
+WHERE
+  "tinggi_badan" IS NULL
+  OR "berat_badan" IS NULL;
