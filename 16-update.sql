89ac9312eb1094bb99b6a08b525594477169e796
Fadhil <fadhil@incit.org>
2025-06-07 14:50:12 +0700

    Menambahkan file materi ke-16

---
+++ 16-update.sql
@@ -0,0 +1,26 @@
+/* 1. Buat data siswa baru sebagai berikut:
+    - nomor induk = 10011
+    - nama lengkap = Rioo
+    - jenis_kelamin = pria
+    - alamat = Jl. Semangka
+    - berat badan = 49 kg
+    - tinggi badan = 155 kg
+    - tempat lahir = Bogor
+    - tanggal lahir = 30 Mei 2010
+ */
+
+ /* 2. Namun, setelah data pada no.1 dimasukkan, terdapat kesalahan penilisan nama yang seharusnya 'Rio'. Perbaiki data tersebut */
+
+/* 3. Lengkapi data siswa yang bernama Andi,telah diketahui alamat tempat tinggal di Jl.Pahlawan dan tinggi badan setelah pemeriksaan kesehatan adalah 145 cm. */
+
+/* 4. Lengkapi alamat siswa sebagai berikut:
+    - Elyn dan Chandra bertempat tinggal di Jl.Kutilang
+    - Fifi bertempat tinggal di Jl.Nangka
+ */
+
+/* 5. Lengkapi data berat badan dan tinggi badan sebagai berikut:
+    - Andi = 160 cm
+    - Ani = 55 kg
+    - Dony = 46 kg dan 157 cm
+    - Chandra = 59 kg dan 155 cm
+    - Fifi = 58 kg */
