1dc7ddfc454d1700a26efe5cf87c4ec18e54ae73
Fadhil <fadhil@incit.org>
2025-12-05 18:34:50 +0700

    Update kode program untuk materi 06c dan 06d

--- lanjutan/06-conditional-expressions.sql
+++ lanjutan/06-conditional-expressions.sql
@@ -17,18 +17,41 @@ SELECT
 FROM "Siswa";
 
 -- 3. NULLIF
-SELECT 
-  "nomor_induk",
-  "nama_lengkap",
-  NULLIF("alamat", '') AS "alamat"
-FROM "Siswa"
-ORDER BY "nomor_induk";
-
--- 4. GREATEST and LEAST
 SELECT
   "nama_lengkap",
-  "berat_badan",
-  "tinggi_badan",
-  GREATEST("berat_badan")
-FROM
-  "Siswa";
+  NULLIF("alamat", '') AS "alamat"
+FROM "Siswa";
+
+-- 4. GREATEST and LEAST
+CREATE TABLE "NilaiRaporSiswa" (
+  "id" SERIAL NOT NULL PRIMARY KEY,
+  "nomor_induk_siswa" VARCHAR(10) NOT NULL REFERENCES "Siswa"("nomor_induk"),
+  "nilai_1" INT,
+  "nilai_2" INT,
+  "nilai_3" INT,
+  "nilai_4" INT,
+  "nilai_5" INT,
+  "nilai_6" INT
+);
+
+SELECT * FROM "Siswa" ORDER BY "nomor_induk";
+
+INSERT INTO "NilaiRaporSiswa" ("nomor_induk_siswa", "nilai_1", "nilai_2", "nilai_3", "nilai_4", "nilai_5", "nilai_6")
+VALUES 
+  ('10004', 80, 92, 75, 78, 80, 40),
+  ('10005', 70, 88, 45, 79, 50, 90),
+  ('10006', 90, 78, 65, 48, 55, 80),
+  ('10007', 83, 60, 95, 78, 89, 45),
+  ('10008', 81, 50, 85, 68, 83, 42);
+
+SELECT * FROM "NilaiRaporSiswa";
+
+SELECT
+  "nomor_induk_siswa",
+  GREATEST("nilai_1", "nilai_2", "nilai_3", "nilai_4", "nilai_5", "nilai_6") AS "nilai_tertinggi",
+  LEAST("nilai_1", "nilai_2", "nilai_3", "nilai_4", "nilai_5", "nilai_6") AS "nilai_terendah"
+FROM "NilaiRaporSiswa";
+
+-- Fungsi agregasi: mengagregatkan seluruh data
+MIN()
+MAX()
